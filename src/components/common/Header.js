d73428bd3968ce1b396c2c3c365b91156fd229b6
Leath Cooper <leathcooper@gmail.com>
2018-04-27 01:56:50 -0700

    add an icon

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,6 +4,8 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import SvgIcon from 'material-ui/SvgIcon';
 
 
 const styles = {
@@ -18,10 +20,19 @@ const propTypes = {
   }).isRequired,
 };
 
+const BlackstarIcon = props => (
+  <SvgIcon {...props} >
+    <path d='m55,237 74-228 74,228L9,96h240'/>
+  </SvgIcon>
+)
+
 export const HeaderComponent = ({ classes }) => (
   <div className={classes.root} >
     <AppBar position="static" color="default">
       <Toolbar>
+        <IconButton className={classes.blackstar} color="inherit" aria-label="Bowie Baby">
+          <BlackstarIcon viewBox='0 0 260 260' nativeColor='#000' />
+        </IconButton>
         <Typography variant="title" color="inherit">
             Bowie Baby
         </Typography>
